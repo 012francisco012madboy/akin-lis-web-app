c9e1297a5b363f2d3ff1e24c7f0de9efdeb77fd0
Mário Salvador <mariopaulobunga@gmail.com>
2025-01-15 21:24:20 +0100

    feat: :sparkles: Adiciona seleção de dispositivos de câmera e melhora o gerenciamento de erros no componente AutomatedAnalysis

--- src/app/akin/patient/[id]/ready-exam/modalAutomatiImage.tsx
+++ src/app/akin/patient/[id]/ready-exam/modalAutomatiImage.tsx
@@ -1,11 +1,20 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from "@/components/ui/dialog";
+import CustomCamera from "@/app/akin/camera/camera";
 
-export default function AutomatedAnalysis({ isAutomatedAnalysisOpen, setIsAutomatedAnalysisOpen }: { isAutomatedAnalysisOpen: boolean,setIsAutomatedAnalysisOpen: (value: boolean) => void }) {
+export default function AutomatedAnalysis({ isAutomatedAnalysisOpen, setIsAutomatedAnalysisOpen }: { isAutomatedAnalysisOpen: boolean, setIsAutomatedAnalysisOpen: (value: boolean) => void }) {
+
+  const cameraRef = useRef<{
+    captureImage: () => void;
+    stopCamera: () => void;
+  }>(null);
+  const [devices, setDevices] = useState<MediaDeviceInfo[]>([]);
+  const [currentImage, setCurrentImage] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
   const [isCapturing, setIsCapturing] = useState(false);
   const [timer, setTimer] = useState(1);
   const [capturedImages, setCapturedImages] = useState<string[]>([]);
@@ -31,6 +40,15 @@ export default function AutomatedAnalysis({ isAutomatedAnalysisOpen, setIsAutoma
     setIsCapturing(false);
     setMessage("Captura finalizada. Escolha como enviar as imagens.");
   };
+  useEffect(() => {
+    if (!devices.length) {
+
+      setError("Nenhuma câmera detectada. Certifique-se de que a câmera está conectada.");
+    } else {
+      setError(null);
+    }
+  }, [devices]);
+
 
   useEffect(() => {
     let interval: NodeJS.Timeout;
@@ -41,7 +59,7 @@ export default function AutomatedAnalysis({ isAutomatedAnalysisOpen, setIsAutoma
       }, 1000);
     }
     return () => clearInterval(interval);
-  }, [isCapturing, timer, capturedImages.length,handleCaptureImage]);
+  }, [isCapturing, timer, capturedImages.length, handleCaptureImage]);
 
 
   return (
@@ -50,20 +68,60 @@ export default function AutomatedAnalysis({ isAutomatedAnalysisOpen, setIsAutoma
         <div className="flex flex-col h-full ">
           <header className="bg-white shadow py-4 px-5 flex justify-between items-center rounded-md">
             <h1 className="text-lg font-semibold">Análise Automatizada</h1>
+            <select
+              onChange={(e) => {
+                const selectedDevice = devices.find(
+                  (device) => device.deviceId === e.target.value
+                );
+                if (selectedDevice) setDevices([selectedDevice]);
+              }}
+              value={devices[0]?.deviceId ? devices[0]?.deviceId : "Sem câmeras detectadas"}
+              className="px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            >
+              {devices.length > 0 ? (
+                devices.map((device) => (
+                  <option key={device.deviceId} value={device.deviceId}>
+                    {device.label || `Câmera ${device.deviceId}`}
+                  </option>
+                ))
+              ) : (
+                <option disabled>Sem câmeras detectadas</option>
+              )}
+            </select>
             <Button onClick={handleStartCapturing} disabled={isCapturing}>
               Iniciar Captura
             </Button>
+
           </header>
-          <section className="mt-6 overflow-y-auto h-full max-h-[500px] shadow-md rounded-md">
-            <h2 className="text-xl font-bold mb-2">Imagens Capturadas ({capturedImages.length}/{maxCaptures})</h2>
+          <section className="mt-6 overflow-y-auto h-full max-h-[500px] shadow-md rounded-md pb-10">
+            <div className="w-[500px] h-[500px]">
+              <CustomCamera
+                ref={cameraRef}
+                getCapturedImage={(img) => setCurrentImage(img)}
+                getAllVideoDevices={setDevices}
+                className="h-full w-full"
+                videoClassName="h-full w-full"
+              />
+            </div>
+
+            <h2 className="text-xl font-bold mb-2 mt-3">Imagens Capturadas ({capturedImages.length}/{maxCaptures})</h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-              {capturedImages.map((image, idx) => (
+              {capturedImages.map((image, index) => (
                 // eslint-disable-next-line @next/next/no-img-element
-                <img
-                  key={idx}
-                  src={image}
-                  alt={`Captura ${idx}`}
+                // <img
+                //   key={index}
+                //   src={image}
+                //   alt={`Captura ${index}`}
+                //   className="w-full h-40 object-cover rounded-md border"
+                // />
+                <Image
+                  width={500}
+                  height={500}
                   className="w-full h-40 object-cover rounded-md border"
+                  key={index}
+                  src={image}
+                  alt={`Imagem ${index + 1}`}
+                  objectFit="contain"
                 />
               ))}
             </div>
