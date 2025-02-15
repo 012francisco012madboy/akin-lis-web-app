a686cd5cc5a60b5a4ececbaf803045ec37298109
Mário Salvador <mariopaulobunga@gmail.com>
2025-02-16 00:24:24 +0100

    feat: :sparkles: Adiciona link para o perfil do usuário no menu do sidebar e melhora a navegação

--- src/components/layout/sidebarConfig/nav-user.tsx
+++ src/components/layout/sidebarConfig/nav-user.tsx
@@ -17,15 +17,16 @@ import { useAuthStore } from "@/utils/zustand-store/authStore";
 import { useQuery } from "@tanstack/react-query"
 import { _axios } from "@/lib/axios"
 import { UserData } from "@/app/akin/profile/page"
+import Link from "next/link"
 
 export function NavUser({
-  
+
 }: {
- 
-}) {
+
+  }) {
   const { isMobile } = useSidebar();
   const { user } = useAuthStore()
-  const { data:loggedUser, isPending } = useQuery({
+  const { data: loggedUser, isPending } = useQuery({
     queryKey: ['user-data'],
     queryFn: async () => {
       return await _axios.get<UserData>(`/users/${user?.id}`);
@@ -52,13 +53,13 @@ export function NavUser({
             </SidebarMenuButton>
           </DropdownMenuTrigger>
           <DropdownMenuContent
-            className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+            className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg cursor-pointer"
             side={isMobile ? "bottom" : "right"}
             align="end"
             sideOffset={4}
           >
-            <DropdownMenuLabel className="p-0 font-normal">
-              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+            <DropdownMenuLabel className="p-0 font-normal"              >
+              <Link href={'/akin/profile'} className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
                   <AvatarImage src={""} alt={loggedUser?.data.nome} />
                   <AvatarFallback className="rounded-lg">{loggedUser?.data.nome.slice(0, 2).charAt(0)}</AvatarFallback>
@@ -67,10 +68,14 @@ export function NavUser({
                   <span className="truncate font-semibold">{loggedUser?.data.nome}</span>
                   <span className="truncate text-xs">{loggedUser?.data.email}</span>
                 </div>
-              </div>
+              </Link>
             </DropdownMenuLabel>
             <DropdownMenuSeparator />
-            <DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer" onClick={
+              () => {
+                window.location.href = '/logout'
+              }
+            }>
               <LogOut />
               Log out
             </DropdownMenuItem>
