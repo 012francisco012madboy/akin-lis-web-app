b93b84e8baacf6ee87b4106ae34515dcf743db19
osapicare-acc <143323266+osapicare-acc@users.noreply.github.com>
2025-06-17 01:36:26 +0000

    dep2

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,13 +1,6 @@
 // /pages/cadastro/index.tsx
-import { useEffect } from "react";
-import { useRouter } from "next/router";
 
 export default function CadastroIndex() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push("/cadastro/step1");
-  }, []);
 
   return null;
 }
